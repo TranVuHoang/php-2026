8cf7bb676bb80b17e74f8345227335444122c121
TranVuHoang <tranvuhoangjr@gmail.com>
2026-04-23 14:50:27 +0700

    bai 12 cac loai toan tu trong php phan 2

---
+++ php-hienu/2-php/12-toan-tu.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 2;
+$b ='2';
+
+# Toán tử ==
+var_dump($a == $b); // bool(true)
+echo "<br />";
+
+# Toán tử ===
+var_dump($a === $b); // bool(false)
+echo "<br />";
+
+# Toán tử !=
+var_dump($a != $b); // bool(false)
+echo "<br />";
+
+# Toán tử !==
+var_dump($a !== $b); // bool(true)
+echo "<br />";
